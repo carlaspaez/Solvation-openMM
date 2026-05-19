5e11568cf4244eb25962f1592f64e6518e28abcb
carlaspaez <carla.soler.paez@uvic.cat>
2026-05-20 00:55:23 +0200

    canvis

--- scrips/enviar.sh
+++ scrips/enviar.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=2:00:00
 ##SBATCH --cpus-per-task=4
 ##SBATCH --mem=4GB
-
+#SBATCH --partition=normal2                 # canvieu de node, tenim highmem, normal1,2,3,4,5 i gpu
 set -euo pipefail
 
 if [ -z "${1:-}" ]; then
